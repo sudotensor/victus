b844383e9fa27d34a8b825a65797582a969d497b
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2020-11-15 11:54:01 +0000

    Initial Commit

---
+++ Victus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Victus
+//
+//  Created by Sudarshan Sreeram on 15/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Victus/VictusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VictusApp.swift
+//  Victus
+//
+//  Created by Sudarshan Sreeram on 15/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct VictusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
