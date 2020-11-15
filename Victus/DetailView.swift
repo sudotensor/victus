65b8846968fd904da780173be7ce0db91741303c
Sudarshan Sreeram <sudarshan.sreeram19@imperial.ac.uk>
2020-11-15 19:05:01 +0000

    Add food data structure

---
+++ Victus/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Victus
+//
+//  Created by Sudarshan Sreeram on 15/11/2020.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
---
+++ Victus/FoodData.swift
@@ -0,0 +1,22 @@
+//
+//  FoodData.swift
+//  Victus
+//
+//  Created by Sudarshan Sreeram on 15/11/2020.
+//
+
+import Foundation
+
+struct FoodData {
+    /* Nutritional Information */
+    public var calories: String
+    public var protien: String
+    public var fat: String
+    public var carbs: String
+    public var fiber: String
+    
+    /* Better Alternatives */
+    public var immunity: String
+    public var vegan: String
+    public var keto: String
+}
